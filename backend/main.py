7cab678d447e97782aa8ae425fae68b118f63b08
geer26 <gergo.kurucz@gmail.com>
2022-04-08 20:25:49 +0200

    v0.1

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
 from backend.core.settings import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 
-@app.get("/")
-def hello_api():
-    return {"msg":"Hello API -2"}
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	return app
+
+
+app = start_application()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
