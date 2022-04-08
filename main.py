3f89ddd90627b070c2a7789bf0ce722c7f6f16cb
geer26 <gergo.kurucz@gmail.com>
2022-04-08 08:33:45 +0200

    v0.1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+#from dotenv import load_dotenv
+from settings import settings
 
+app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+#app = FastAPI()
+
+@app.get("/")
+def hello_api():
+    return {"msg":"Hello API -2"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
