2d3273e7d010bce3af46ae3a5c8ad243d12c1939
geer26 <gergo.kurucz@gmail.com>
2022-04-12 18:43:49 +0200

    v0.1

--- backend/db/repository/users.py
+++ backend/db/repository/users.py
@@ -5,7 +5,7 @@ from db.models.users import User
 from core.hashing import Hasher
 
 
-def create_new_user(user:UserCreate,db:Session):
+def create_new_user(user: UserCreate, db: Session):
     user = User(username=user.username,
         email = user.email,
         hashed_password=Hasher.get_password_hash(user.password),
