48b9970c7c095d70fed61745f764bb45d3da4a8f
geer26 <gergo.kurucz@gmail.com>
2022-04-13 12:37:53 +0200

    v0.1

--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -4,7 +4,7 @@ from schemas.jobs import JobCreate
 from db.models.jobs import Job
 
 
-def create_new_job(job: JobCreate,db: Session,owner_id:int):
+def create_new_job(job: JobCreate, db: Session, owner_id:int):
     job_object = Job(**job.dict(),owner_id=owner_id)
     db.add(job_object)
     db.commit()
