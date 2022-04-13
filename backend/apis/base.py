8a1f71586d2b5606db77f343e737db5c8d255c37
geer26 <gergo.kurucz@gmail.com>
2022-04-13 12:31:18 +0200

    v0.1

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from .version1 import route_general_pages
-from .version1 import route_user
+from .version1 import route_user, route_jobs
 
 
 api_router = APIRouter()
 api_router.include_router(route_general_pages.general_pages_router,prefix="",tags=["general_pages"])
 api_router.include_router(route_user.router,prefix="/users",tags=["users"])
+api_router.include_router(route_jobs.router,prefix="/jobs",tags=["jobs"])
